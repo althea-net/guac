121ca89ea640becc8125a172333c515280723ca7
Jehan <jehan.tremback@gmail.com>
2017-07-04 00:07:47 -0700

    cleanup

--- contracts/PaymentChannels.sol
+++ contracts/PaymentChannels.sol
@@ -1,49 +1,86 @@
 pragma solidity ^0.4.11;
 import "ECVerify.sol";
+import './zeppelin/token/StandardToken.sol';
 
 
 contract PaymentChannels is ECVerify, StandardToken {
     mapping (bytes32 => Channel) channels;
 
-    modifier channelDoesNotExist (channelId) {
-        require(channels[channelId].channelId != channelId);
+    modifier channelDoesNotExist (bytes32 _channelId) {
+        require(channels[_channelId].channelId != _channelId);
         _;
     }
 
-    modifier channelExists (channelId) {
-        require(channels[channelId].channelId == channelId);
+    modifier channelExists (bytes32 _channelId) {
+        require(channels[_channelId].channelId == _channelId);
         _;
     }
 
-    modifier channelIsOpen (channelId) {
-        require(channels[channelId].open);
+    modifier channelIsOpen (bytes32 _channelId) {
+        require(channels[_channelId].open);
         _;
     }
 
-    modifier channelIsFunded (channelId) {
-        require(channels[channelId].funded);
+    modifier channelIsFunded (bytes32 _channelId) {
+        require(channels[_channelId].funded);
         _;
     }
 
-    modifier channelIsSettled (channelId) {
+    modifier channelIsSettled (bytes32 _channelId) {
         require(
-            !channels[channelId].open &&
-            block.number >= channels[channelId].settlingBlock
+            !channels[_channelId].open &&
+            block.number >= channels[_channelId].settlingBlock
         );
         _;
     }
 
-    modifier channelIsNotSettled (channelId) {
+    modifier channelIsNotSettled (bytes32 _channelId) {
         require(
-            channels[channelId].open ||
-            block.number < channels[channelId].settlingBlock
+            channels[_channelId].open ||
+            block.number < channels[_channelId].settlingBlock
         );
         _;
     }
 
-    modifier sequenceNumberIsHighest (channelId) {
-        require(sequenceNumber > channels[channelId].sequenceNumber);
+    modifier balancesEqualTotal (bytes32 _channelId, uint256 _balance0, uint256 _balance1) {
+        require(_balance0.add(_balance1) == channels[_channelId].totalBalance);
         _;
+    }
+
+    modifier sequenceNumberIsHighest (bytes32 _channelId) {
+        require(sequenceNumber > channels[_channelId].sequenceNumber);
+        _;
+    }
+
+    function signedByBoth (
+        bytes32 _fingerprint, 
+        bytes32 _signature0, 
+        bytes32 _signature1, 
+        bytes32 _channelId
+    ) {
+        require(
+            ecverify(_fingerprint, _signature0, channels[_channelId].address0) &&
+            ecverify(_fingerprint, _signature1, channels[_channelId].address1)
+        );
+    }
+
+    function signedByOne (
+        bytes32 _fingerprint,
+        bytes32 _signature,
+        bytes32 _channelId
+    ) {
+        require(
+            ecverify(_fingerprint, _signature, channels[_channelId].address0) ||
+            ecverify(_fingerprint, _signature, channels[_channelId].address1)
+        );
+    }
+
+    function incrementBalance(address _addr, uint _value) {
+        balances[_addr] = balances[_addr].add(_value);
+    }
+
+    function decrementBalance(address _addr, uint _value) {
+        balances[_addr] = balances[_addr].sub(_value);
     }
 
     struct Channel {
@@ -53,16 +90,16 @@ contract PaymentChannels is ECVerify, StandardToken {
 
         bool open;
         bool funded;
-        uint settlingPeriod;
-        uint settlingBlock;
+        uint256 settlingPeriod;
+        uint256 settlingBlock;
 
         uint256 balance0;
         uint256 balance1;
 
-        uint sequenceNumber;
+        uint256 sequenceNumber;
     }
 
-    function getChannel(bytes32 channelId) returns(
+    function getChannel(bytes32 _channelId) returns(
         address address0,
         address address1,
 
@@ -73,168 +110,180 @@ contract PaymentChannels is ECVerify, StandardToken {
 
         uint256 balance0,
         uint256 balance1,
+        uint256 totalBalance,
 
         uint sequenceNumber
     ) {
-        address0 = channels[channelId].address0;
-        address1 = channels[channelId].address1;
-        open = channels[channelId].open;
-        settlingPeriod = channels[channelId].settlingPeriod;
-        settlingBlock = channels[channelId].settlingBlock;
-        state = channels[channelId].state;
-        sequenceNumber = channels[channelId].sequenceNumber;
+        address0 = channels[_channelId].address0;
+        address1 = channels[_channelId].address1;
+        open = channels[_channelId].open;
+        settlingPeriod = channels[_channelId].settlingPeriod;
+        settlingBlock = channels[_channelId].settlingBlock;
+        state = channels[_channelId].state;
+        sequenceNumber = channels[_channelId].sequenceNumber;
     }
 
     function newChannel(
-        bytes32 channelId,
+        bytes32 _channelId,
 
-        address address0,
-        address address1,
+        address _address0,
+        address _address1,
 
-        uint256 balance0,
-        uint256 balance1,
+        uint256 _balance0,
+        uint256 _balance1,
 
-        uint256 settlingPeriod,
+        uint256 _settlingPeriod,
 
-        bytes signature0,
-        bytes signature1
-    ) channelDoesNotExist(channelId)
+        bytes _signature0,
+        bytes _signature1
+    ) channelDoesNotExist(_channelId)
     {
         bytes32 fingerprint = sha3(
             "newChannel",
-            channelId,
+            _channelId,
 
-            address0,
-            address1,
+            _address0,
+            _address1,
 
-            balance0,
-            balance1,
+            _balance0,
+            _balance1,
 
-            settlingPeriod
+            _settlingPeriod
         );
 
-        require(ecverify(fingerprint, signature0, address0));
-        require(ecverify(fingerprint, signature1, address1));
+        signedByBoth(
+            fingerprint,
+            _signature0,
+            _signature1,
+            _channelId
+        );
 
-        super.decrementBalance(address0, balance0);
-        super.decrementBalance(address1, balance1);
+        decrementBalance(_address0, _balance0);
+        decrementBalance(_address1, _balance1);
+
+        uint256 totalBalance = _balance0.add(_balance1);
 
         Channel memory channel = Channel(
-            channelId,      // bytes32 channelId;
-            address0,       // address address0;
-            address1,       // address address1;
+            _channelId,      // bytes32 channelId;
+            _address0,       // address address0;
+            _address1,       // address address1;
             
-            true,           // bool open;
-            true,           // bool funded;
-            settlingPeriod, // uint16 settlingPeriod;
-            0,              // uint16 settlingBlock;
+            true,            // bool open;
+            true,            // bool funded;
+            _settlingPeriod, // uint16 settlingPeriod;
+            0,               // uint16 settlingBlock;
 
-            balance0,       // uint256 balance0;
-            balance1,       // uint256 balance1;
+            _balance0,       // uint256 balance0;
+            _balance1,       // uint256 balance1;
+            totalBalance,    // uint256 totalBalance;
 
-            0               // uint16 sequenceNumber;
+            0                // uint16 sequenceNumber;
         );
 
-        channels[channelId] = channel;
+        channels[_channelId] = channel;
     }
 
     function updateBalances(
-        bytes32 channelId,
-        uint256 sequenceNumber,
+        bytes32 _channelId,
+        uint256 _sequenceNumber,
 
-        uint256 balance0,
-        uint256 balance1,
+        uint256 _balance0,
+        uint256 _balance1,
 
-        bytes signature0,
-        bytes signature1
-    ) channelExists(channelId)
-      channelIsNotSettled(channelId)
-      sequenceNumberIsHighest(channelId, sequenceNumber)
+        bytes _signature0,
+        bytes _signature1
+    ) channelExists(_channelId)
+      channelIsNotSettled(_channelId)
+      sequenceNumberIsHighest(_channelId, _sequenceNumber)
+      balancesEqualTotal(_channelId, _balance0, _balance1)
     {
         bytes32 fingerprint = sha3(
             "updateBalances",
-            channelId,
-            sequenceNumber,
-            balance0,
-            balance1
+            _channelId,
+            _sequenceNumber,
+            _balance0,
+            _balance1
         );
 
-        require(ecverify(fingerprint, signature0, channels[channelId].address0));
-        require(ecverify(fingerprint, signature1, channels[channelId].address1));
+        signedByBoth(
+            fingerprint,
+            _signature0,
+            _signature,
+            _channelId
+        );
 
-        channels[channelId].balance0 = balance0;
-        channels[channelId].balance1 = balance1;
-        channels[channelId].sequenceNumber = sequenceNumber;
+        channels[_channelId].balance0 = _balance0;
+        channels[_channelId].balance1 = _balance1;
+        channels[_channelId].sequenceNumber = _sequenceNumber;
     }
 
     function updateBalancesHashlock(
-        bytes32 channelId,
-        uint256 sequenceNumber,
+        bytes32 _channelId,
+        uint256 _sequenceNumber,
 
-        uint256 balance0,
-        uint256 balance1,
+        uint256 _balance0,
+        uint256 _balance1,
 
-        bytes32 hashed,
-        bytes preimage,
+        bytes32 _hashed,
+        bytes _preimage,
 
-        bytes signature0,
-        bytes signature1
-    ) channelExists(channelId)
-      channelIsNotSettled(channelId)
-      sequenceNumberIsHighest(channelId, sequenceNumber)
+        bytes _signature0,
+        bytes _signature1
+    ) channelExists(_channelId)
+      channelIsNotSettled(_channelId)
+      sequenceNumberIsHighest(_channelId, _sequenceNumber)
+      balancesEqualTotal(_channelId, _balance0, _balance1)
     {
         bytes32 fingerprint = sha3(
             "updateBalancesHashlock",
-            channelId,
-            sequenceNumber,
-            hashed,
-            balance0,
-            balance1
+            _channelId,
+            _sequenceNumber,
+            _hashed,
+            _balance0,
+            _balance1
         );
 
-        require(ecverify(fingerprint, signature0, channels[channelId].address0));
-        require(ecverify(fingerprint, signature1, channels[channelId].address1));
+        signedByBoth(
+            fingerprint,
+            _signature0,
+            _signature,
+            _channelId
+        );
 
-        require(sha3(preimage) == hashed)
+        require(sha3(_preimage) == _hashed);
 
-        channels[channelId].balance0 = balance0;
-        channels[channelId].balance1 = balance1;
-        channels[channelId].sequenceNumber = sequenceNumber;
+        channels[_channelId].balance0 = _balance0;
+        channels[_channelId].balance1 = _balance1;
+        channels[_channelId].sequenceNumber = _sequenceNumber;
     }
 
     function closeChannel (
-        bytes32 channelId,
-        bytes signature,
-        address signer
-    ) channelExists(channelId)
-      channelIsOpen(channelId)
+        bytes32 _channelId,
+        bytes _signature
+    ) channelExists(_channelId)
+      channelIsOpen(_channelId)
     {
         bytes32 fingerprint = sha3(
             "closeChannel",
-            channelId
+            _channelId
         );
 
-        if (signer == channels[channelId].address0) {
-            require(ecverify(fingerprint, signature, channels[channelId].address0));
-        } else if (signer == channels[channelId].address1) {
-            require(ecverify(fingerprint, signature, channels[channelId].address1));
-        } else {
-            throw;
-        }
+        signedByOne(fingerprint, _signature, _channelId);
 
-        channels[channelId].open = false;
-        channels[channelId].settlingBlock = block.number + channels[channelId].settlingPeriod;
+        channels[_channelId].open = false;
+        channels[_channelId].settlingBlock = block.number + channels[channelId].settlingPeriod;
     }
 
     function withdraw(
-        bytes32 channelId
-    ) channelExists(channelId)
-      channelIsSettled(channelId)
-      channelIsFunded(channelId)
+        bytes32 _channelId
+    ) channelExists(_channelId)
+      channelIsSettled(_channelId)
+      channelIsFunded(_channelId)
     {
-        channels[channelId].funded = false;
+        channels[_channelId].funded = false;
+        channels[_channelId].totalBalance = 0;
 
-        super.incrementBalance(address0, balance0);
-        super.incrementBalance(address1, balance1);
+        incrementBalance(channels[_channelId].address0, channels[_channelId].balance0);
+        incrementBalance(channels[_channelId].address1, channels[_channelId].balance1);
     }
 }
