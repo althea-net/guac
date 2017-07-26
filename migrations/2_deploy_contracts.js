d855e40ec725b10c45c9000fabcaeba62934ff5f
Jehan <jehan.tremback@gmail.com>
2017-07-26 13:48:50 -0700

    get rid of old contracts etc

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,9 +2,6 @@ var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
 var PaymentChannels = artifacts.require("PaymentChannels");
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+module.exports = function (deployer) {
   deployer.deploy(PaymentChannels);
 };
