a7f38b59ad4674dc28de40a599579658e5bb1c33
Jehan <jehan.tremback@gmail.com>
2017-07-27 15:34:36 -0700

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,3 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
 var PaymentChannels = artifacts.require("PaymentChannels");
 
 module.exports = function (deployer) {
