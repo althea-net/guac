f46f5c67a30554635d8ecdd7e9e35d8f196d1ddc
ScottWorks <cescott.ieee@gmail.com>
2017-08-01 22:59:20 -0400

    added 'createChannel' function to unit tests as neeed

--- test/newChannel.js
+++ test/newChannel.js
@@ -1,260 +1,148 @@
 // cook mango twist then skin sort option civil have still rather guilt
 
+
 const test = require('blue-tape')
 const p = require('util').promisify
 
 const {
-  ACCT_0_PRIVKEY,
-  ACCT_1_PRIVKEY,
-  ACCT_0_ADDR,
-  ACCT_1_ADDR
+    ACCT_0_PRIVKEY,
+    ACCT_1_PRIVKEY,
+    ACCT_0_ADDR,
+    ACCT_1_ADDR
 } = require('./constants.js')
 
 const {
-  filterLogs,
-  takeSnapshot,
-  revertSnapshot,
-  solSha3,
-  sign
+    createChannel,
+    filterLogs,
+    takeSnapshot,
+    revertSnapshot,
+    solSha3,
+    sign
 } = require('./utils.js')
 
-module.exports = async (test, instance) => {
-  test('newChannel happy path', async t => {
-    const snapshot = await takeSnapshot()
-    const eventLog = instance.allEvents()
+module.exports = async(test, instance) => {
 
-    await instance.mint(ACCT_0_ADDR, 12)
-    await instance.mint(ACCT_1_ADDR, 12)
-      
-    const channelId = '0x1000000000000000000000000000000000000000000000000000000000000000'
+    test('newChannel happy path', async t => {
+        const snapshot = await takeSnapshot()
+        const eventLog = instance.allEvents()
 
-    const address0 = ACCT_0_ADDR
-    const address1 = ACCT_1_ADDR
+        const channelId = '0x1000000000000000000000000000000000000000000000000000000000000000'
+        const string = 'newChannel'
 
-    const balance0 = 6
-    const balance1 = 6
+        await createChannel(
+            instance,
+            string,
+            channelId,
 
-    const settlingPeriod = 2
+            6,
+            6,
 
-    const fingerprint = solSha3(
-      'newChannel',
-      channelId,
+            2
+        )
 
-      address0,
-      address1,
 
-      balance0,
-      balance1,
+        t.equal((await instance.balanceOf(ACCT_0_ADDR)).c[0], 6)
+        t.equal((await instance.balanceOf(ACCT_1_ADDR)).c[0], 6)
 
-      settlingPeriod
-    )
+        t.deepEqual(
+            JSON.parse(JSON.stringify(await instance.channels(channelId))), ['0x1000000000000000000000000000000000000000000000000000000000000000',
 
-    const signature0 = sign(fingerprint, new Buffer(ACCT_0_PRIVKEY, 'hex'))
-    const signature1 = sign(fingerprint, new Buffer(ACCT_1_PRIVKEY, 'hex'))
+                '0xa09bd41a9f1d469fca7b3f82a579b855dd6b279d',
+                '0x25e27882eeb2159ad3164ed2622241740dfe0528',
 
-    await instance.newChannel(
-      channelId,
+                false, false,
 
-      address0,
-      address1,
+                '2', '0',
 
-      balance0,
-      balance1,
+                '6', '6', '12',
 
-      settlingPeriod,
+                '0x', '0'
+            ]
+        )
 
-      signature0,
-      signature1
-    )
+        const logs = await p(eventLog.get.bind(eventLog))()
+        console.log('logs', filterLogs(logs))
+        eventLog.stopWatching()
 
-    t.equal((await instance.balanceOf(ACCT_0_ADDR)).c[0], 6)
-    t.equal((await instance.balanceOf(ACCT_0_ADDR)).c[0], 6)
+        await revertSnapshot(snapshot)
 
-    t.deepEqual(
-      JSON.parse(JSON.stringify(await instance.channels(channelId))),
-      [ '0x1000000000000000000000000000000000000000000000000000000000000000',
-        
-        '0xa09bd41a9f1d469fca7b3f82a579b855dd6b279d',
-        '0x25e27882eeb2159ad3164ed2622241740dfe0528',
-        
-        false, false,
-        
-        '2', '0',
-        
-        '6', '6', '12',
-        
-        '0x', '0' ]
-    )
+    })
 
-    const logs = await p(eventLog.get.bind(eventLog))()
-    console.log('logs', filterLogs(logs))
-    eventLog.stopWatching()
 
-    await revertSnapshot(snapshot)
-  })
+    test('newChannel bad sig', async t => {
+        const snapshot = await takeSnapshot()
 
-  test('newChannel bad sig', async t => {
-    const snapshot = await takeSnapshot()
+        const channelId = '0x1000000000000000000000000000000000000000000000000000000000000000'
+        const string = 'newChannel derp'
 
-    await instance.mint(ACCT_0_ADDR, 12)
-    await instance.mint(ACCT_1_ADDR, 12)
-      
-    const channelId = '0x1000000000000000000000000000000000000000000000000000000000000000'
+        t.shouldFail(createChannel(
+            instance,
+            string,
+            channelId,
 
-    const address0 = ACCT_0_ADDR
-    const address1 = ACCT_1_ADDR
+            6,
+            6,
 
-    const balance0 = 6
-    const balance1 = 6
+            2
+        ))
 
-    const settlingPeriod = 2
+        await revertSnapshot(snapshot)
 
-    const fingerprint = solSha3(
-      'newChannel derp',
-      channelId,
+    })
 
-      address0,
-      address1,
 
-      balance0,
-      balance1,
 
-      settlingPeriod
-    )
+    test('newChannel bad amount', async t => {
+        const snapshot = await takeSnapshot()
 
-    const signature0 = sign(fingerprint, new Buffer(ACCT_0_PRIVKEY, 'hex'))
-    const signature1 = sign(fingerprint, new Buffer(ACCT_1_PRIVKEY, 'hex'))
+        const channelId = '0x1000000000000000000000000000000000000000000000000000000000000000'
+        const string = 'newChannel'
 
-    t.shouldFail(instance.newChannel(
-      channelId,
+        t.shouldFail(createChannel(
+            instance,
+            string,
+            channelId,
 
-      address0,
-      address1,
+            6,
+            60,
 
-      balance0,
-      balance1,
+            2
+        ))
 
-      settlingPeriod,
+        await revertSnapshot(snapshot)
 
-      signature0,
-      signature1
-    ))
+    })
 
-    await revertSnapshot(snapshot)
-  })
 
-  test('newChannel bad amount', async t => {
-    const snapshot = await takeSnapshot()
+    test('newChannel already exists', async t => {
+        const snapshot = await takeSnapshot()
 
-    await instance.mint(ACCT_0_ADDR, 12)
-    await instance.mint(ACCT_1_ADDR, 12)
-      
-    const channelId = '0x1000000000000000000000000000000000000000000000000000000000000000'
+        const channelId = '0x1000000000000000000000000000000000000000000000000000000000000000'
+        const string = 'newChannel'
 
-    const address0 = ACCT_0_ADDR
-    const address1 = ACCT_1_ADDR
+        createChannel(
+            instance,
+            string,
+            channelId,
 
-    const balance0 = 6
-    const balance1 = 60000
+            6,
+            6,
 
-    const settlingPeriod = 2
+            2
+        )
 
-    const fingerprint = solSha3(
-      'newChannel',
-      channelId,
+        t.shouldFail(createChannel(
+            instance,
+            string,
+            channelId,
 
-      address0,
-      address1,
+            6,
+            6,
 
-      balance0,
-      balance1,
+            2
+        ))
 
-      settlingPeriod
-    )
+        await revertSnapshot(snapshot)
 
-    const signature0 = sign(fingerprint, new Buffer(ACCT_0_PRIVKEY, 'hex'))
-    const signature1 = sign(fingerprint, new Buffer(ACCT_1_PRIVKEY, 'hex'))
-
-    t.shouldFail(instance.newChannel(
-      channelId,
-
-      address0,
-      address1,
-
-      balance0,
-      balance1,
-
-      settlingPeriod,
-
-      signature0,
-      signature1
-    ))
-
-    await revertSnapshot(snapshot)
-  })
-
-  test('newChannel already exists', async t => {
-    const snapshot = await takeSnapshot()
-
-    await instance.mint(ACCT_0_ADDR, 12)
-    await instance.mint(ACCT_1_ADDR, 12)
-      
-    const channelId = '0x1000000000000000000000000000000000000000000000000000000000000000'
-
-    const address0 = ACCT_0_ADDR
-    const address1 = ACCT_1_ADDR
-
-    const balance0 = 6
-    const balance1 = 6
-
-    const settlingPeriod = 2
-
-    const fingerprint = solSha3(
-      'newChannel',
-      channelId,
-
-      address0,
-      address1,
-
-      balance0,
-      balance1,
-
-      settlingPeriod
-    )
-
-    const signature0 = sign(fingerprint, new Buffer(ACCT_0_PRIVKEY, 'hex'))
-    const signature1 = sign(fingerprint, new Buffer(ACCT_1_PRIVKEY, 'hex'))
-
-    await instance.newChannel(
-      channelId,
-
-      address0,
-      address1,
-
-      balance0,
-      balance1,
-
-      settlingPeriod,
-
-      signature0,
-      signature1
-    )
-
-    t.shouldFail(instance.newChannel(
-      channelId,
-
-      address0,
-      address1,
-
-      balance0,
-      balance1,
-
-      settlingPeriod,
-
-      signature0,
-      signature1
-    ))
-
-    await revertSnapshot(snapshot)
-  })
+    })
 }
