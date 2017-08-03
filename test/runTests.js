79272b5edae13a51a89d96405242a06e4ad8dbce
Jehan <jehan.tremback@gmail.com>
2017-08-03 14:50:42 -0700

    add "prettier" formatting

--- test/runTests.js
+++ test/runTests.js
@@ -1,16 +1,15 @@
-const test = require('blue-tape')
+const test = require("blue-tape");
 
-const misc = require('./misc.js')
-const newChannel = require('./newChannel.js')
-const updateState = require('./updateState.js')
-const endChannel = require('./endChannel.js')
-const closeChannel = require('./closeChannel.js')
-const PaymentChannels = artifacts.require('PaymentChannels')
-
-  ; (async () => {
-    const instance = await PaymentChannels.deployed()
-    newChannel(test, instance)
-    updateState(test, instance)
-    endChannel(test, instance)
-    closeChannel(test, instance)
-  })()
+const misc = require("./misc.js");
+const newChannel = require("./newChannel.js");
+const updateState = require("./updateState.js");
+const endChannel = require("./endChannel.js");
+const closeChannel = require("./closeChannel.js");
+const PaymentChannels = artifacts.require("PaymentChannels");
+(async () => {
+  const instance = await PaymentChannels.deployed();
+  newChannel(test, instance);
+  updateState(test, instance);
+  endChannel(test, instance);
+  closeChannel(test, instance);
+})();
