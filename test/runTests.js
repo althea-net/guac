6927f0bed2314fa141b0cdbf16ea4ddc9f247be3
Jehan <jehan.tremback@gmail.com>
2017-08-28 16:43:31 -0700

    cleanup in runTests.js

--- test/runTests.js
+++ test/runTests.js
@@ -1,6 +1,5 @@
 const test = require("blue-tape");
 
-const misc = require("./misc.js");
 const newChannel = require("./newChannel.js");
 const updateState = require("./updateState.js");
 const endChannel = require("./endChannel.js");
@@ -10,7 +9,7 @@ const PaymentChannels = artifacts.require("PaymentChannels");
 (async () => {
   const instance = await PaymentChannels.deployed();
   newChannel(test, instance);
-  // updateState(test, instance);
-  // endChannel(test, instance);
-  // closeChannel(test, instance);
+  updateState(test, instance);
+  endChannel(test, instance);
+  closeChannel(test, instance);
 })();
