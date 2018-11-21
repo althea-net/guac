6b6e0f65e2b84baf0ec68d5f5b77c4eaf70582e0
Sebastian Bolanos <sebasheston@gmail.com>
2018-11-21 16:27:12 +0100

    Init quarantine tests

--- test/runTests.js
+++ test/runTests.js
@@ -1,17 +1,22 @@
-const test = require("blue-tape");
-
+const CONST = require("./constants.js")
+/*
 const newChannel = require("./newChannel.js");
 const updateState = require("./updateState.js");
 const startSettlingPeriod = require("./startSettlingPeriod.js");
 const closeChannel = require("./closeChannel.js");
 const redraw = require("./redraw.js");
+*/
 const PaymentChannels = artifacts.require("../contracts/PaymentChannels.sol");
 
-(async () => {
+
+contract("PaymentChannel", async accounts => {
   const instance = await PaymentChannels.deployed();
+  /*
   newChannel(test, instance);
   updateState(test, instance);
   startSettlingPeriod(test, instance);
   closeChannel(test, instance);
   redraw(test, instance);
-})();
+  */
+  assert(true)
+})
