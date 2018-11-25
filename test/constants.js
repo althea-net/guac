5ef8ccfd9c75217efea10e6fa328e3a9d58f9c01
Sebastian Bolanos <sebasheston@gmail.com>
2018-11-25 18:32:46 +0100

    Update constants

--- test/constants.js
+++ test/constants.js
@@ -1,14 +1,12 @@
 const {Wallet} = require('ethers')
 
+// The only constant that matters
 let MNEMONIC = "cook mango twist then skin sort option civil have still rather guilt"
 
 let path =  "m/44'/60'/0'/0/"
 let ACCT_A = new Wallet.fromMnemonic(MNEMONIC, path + 7).signingKey
 let ACCT_B = new Wallet.fromMnemonic(MNEMONIC, path + 8).signingKey
 let ACCT_C = new Wallet.fromMnemonic(MNEMONIC, path + 9).signingKey
-ACCT_A.address = web3.utils.toChecksumAddress(ACCT_A.address)
-ACCT_B.address = web3.utils.toChecksumAddress(ACCT_B.address)
-ACCT_C.address = web3.utils.toChecksumAddress(ACCT_C.address)
 
 module.exports = {
   ACCT_A,
@@ -16,10 +14,4 @@ module.exports = {
   ACCT_C,
   MNEMONIC,
   ZERO: '0x0000000000000000000000000000000000000000',
-  CHANNEL_STATUS: {
-    OPEN: 0,
-    JOINED: 1,
-    CHALLENGE: 2,
-    CLOSED: 3
-  }
 };
