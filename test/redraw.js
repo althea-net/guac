7706c91757dd80080a270f4dc09ce7c01e37b5ea
Sebastian Bolanos <sebasheston@gmail.com>
2018-11-25 20:05:18 +0100

    Make redraw pass test

--- test/redraw.js
+++ test/redraw.js
@@ -1,106 +1,82 @@
-// cook mango twist then skin sort option civil have still rather guilt
-
-const test = require("blue-tape");
-const p = require("util").promisify;
-
-const {
-  ACCT_0_PRIVKEY,
-  ACCT_0_ADDR,
-  ACCT_1_PRIVKEY,
-  ACCT_1_ADDR,
-  ACCT_2_PRIVKEY,
-  ACCT_2_ADDR
-} = require("./constants.js");
+const PaymentChannels = artifacts.require("PaymentChannels.sol")
+const {reverting } = require("./helpers/shouldFail.js")
+const {ACCT_A, ACCT_B} = require("./constants.js");
 
 const {
-  filterLogs,
   takeSnapshot,
   revertSnapshot,
-  solSha3,
-  sign,
   reDraw,
-  mineBlocks,
   createChannel,
   updateState,
-  startSettlingPeriod
+	finalAsserts,
 } = require("./utils.js");
 
-module.exports = async (test, instance) => {
-  test("reDraw happy path", async t => {
-    const snapshot = await takeSnapshot();
+module.exports = context("Redraw", async () => {
+	let instance, snapshotId
+	before(async () => {
+		instance = await PaymentChannels.new()
+	})
+	beforeEach(async () => {
+		snapshotId = await takeSnapshot()
+	})
+	afterEach(async () => {
+		await revertSnapshot(snapshotId)
+	})
+
+  it("reDraw happy path", async () => {
 
     // create channel with 6, 6 (both parties have 12 to start)
     const tx = await createChannel(instance, 6, 6, 2);
     const channelId = tx.logs[0].args._channelId;
+    assert.equal((await instance.balanceOf.call(ACCT_A.address)).c[0], 6);
+    assert.equal((await instance.balanceOf.call(ACCT_B.address)).c[0], 6);
 
-    t.equal((await instance.balanceOf.call(ACCT_0_ADDR)).c[0], 6);
-    t.equal((await instance.balanceOf.call(ACCT_1_ADDR)).c[0], 6);
-
-    // update channel to 5, 7, then 5, 1, effectively withdrawing 6 for address1,
+    // update channel to 5, 7, then 5, 1,
+		// effectively withdrawing 6 for address1,
     // bringing address1's balance to 12
     const redrawTx = await reDraw(instance, channelId, 1, 5, 7, 5, 1);
 
-    t.equal(redrawTx.logs[0].event, "ChannelReDrawn");
-
-    t.equal((await instance.balanceOf.call(ACCT_0_ADDR)).c[0], 6);
-    t.equal((await instance.balanceOf.call(ACCT_1_ADDR)).c[0], 12);
-
-    t.deepEqual(
-      JSON.parse(JSON.stringify(await instance.channels(channelId))),
-      [ACCT_0_ADDR, ACCT_1_ADDR, "6", "5", "1", "1", "2", false, "0"]
-    );
-
-    await revertSnapshot(snapshot);
+    assert.equal(redrawTx.logs[0].event, "ChannelReDrawn");
+    assert.equal((await instance.balanceOf.call(ACCT_A.address)).c[0], 6);
+    assert.equal((await instance.balanceOf.call(ACCT_B.address)).c[0], 12);
+	
+		finalAsserts({
+			instance,
+			channelId,
+			totalBalance: "6",
+			balance0: "5",
+			balance1: "1",
+			sequenceNumber: "1",
+		})
   });
 
-  test("reDraw oldBalance higher than total", async t => {
-    const snapshot = await takeSnapshot();
-
+  it("reDraw oldBalance higher than total", async () => {
     const tx = await createChannel(instance, 6, 6, 2);
     const channelId = tx.logs[0].args._channelId;
-
-    await t.shouldFail(reDraw(instance, channelId, 1, 5, 50, 5, 1));
-
+    await reverting(reDraw(instance, channelId, 1, 5, 50, 5, 1));
     await reDraw(instance, channelId, 1, 5, 7, 5, 1);
-
-    await revertSnapshot(snapshot);
   });
 
-  test("reDraw newBalance unaffordable", async t => {
-    const snapshot = await takeSnapshot();
-
+  it("reDraw newBalance unaffordable", async () => {
     const tx = await createChannel(instance, 6, 6, 2);
     const channelId = tx.logs[0].args._channelId;
-
-    await t.shouldFail(reDraw(instance, channelId, 1, 5, 7, 5, 100));
-
+    await reverting(reDraw(instance, channelId, 1, 5, 7, 5, 100));
     await reDraw(instance, channelId, 1, 5, 7, 5, 1);
-
-    await revertSnapshot(snapshot);
   });
 
-  test("reDraw old sequence number", async t => {
-    const snapshot = await takeSnapshot();
-
+  it("reDraw old sequence number", async () => {
     const tx = await createChannel(instance, 6, 6, 2);
     const channelId = tx.logs[0].args._channelId;
-
     await updateState(instance, channelId, 3, 5, 7);
-
-    await t.shouldFail(reDraw(instance, channelId, 1, 5, 7, 5, 1));
-
+    await reverting(reDraw(instance, channelId, 1, 5, 7, 5, 1));
     await reDraw(instance, channelId, 4, 5, 7, 5, 1);
-
-    await revertSnapshot(snapshot);
   });
 
-  test("reDraw expired tx", async t => {
-    const snapshot = await takeSnapshot();
-
+  it("reDraw expired tx", async () => {
     const tx = await createChannel(instance, 6, 6, 2);
     const channelId = tx.logs[0].args._channelId;
 
-    await t.shouldFail(
+    await reverting(
       reDraw(
         instance,
         channelId,
@@ -114,18 +90,14 @@ module.exports = async (test, instance) => {
     );
 
     await reDraw(instance, channelId, 1, 5, 7, 5, 1);
-
-    await revertSnapshot(snapshot);
   });
 
-  test("reDraw nonexistant channel", async t => {
-    const snapshot = await takeSnapshot();
-
-    await t.shouldFail(
+  it("reDraw nonexistant channel", async () => {
+    await reverting(
       reDraw(
         instance,
         "0x1000000000000000000000000000000000000000000000000000000000000000",
-        1,
+				1,
         5,
         7,
         5,
@@ -135,9 +107,6 @@ module.exports = async (test, instance) => {
 
     const tx = await createChannel(instance, 6, 6, 2);
     const channelId = tx.logs[0].args._channelId;
-
     await reDraw(instance, channelId, 1, 5, 7, 5, 1);
-
-    await revertSnapshot(snapshot);
   });
-};
+})
